2121685f75783478295c2faa2b4b3886e5644e7d
chayaleA <chayale96@gmail.com>
2024-03-15 13:23:49 +0200

    JWT-

---
+++ Solid.Data/Migrations/20240315104221_ColumnPassword1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Solid.Data.Migrations
+{
+    public partial class ColumnPassword1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ bank-api-project/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using bank_api_project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Solid.Core.Services;
+using Solid.Service;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace bank_api_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration, ICustomerService customerService)
+        {
+            _customerService = customerService;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginModel loginModel)
+        {
+            var customer = _customerService.GetByCustomerNameAndPassword(loginModel.UserName, loginModel.Password);    
+            if (customer is not null)
+            {
+                var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, customer.Name),
+                 new Claim(ClaimTypes.MobilePhone, customer.Phone),
+                new Claim(ClaimTypes.Role, "customer")
+            };
+
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("JWT:Key")));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var tokeOptions = new JwtSecurityToken(
+                    issuer: _configuration.GetValue<string>("JWT:Issuer"),
+                    audience: _configuration.GetValue<string>("JWT:Audience"),
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(6),
+                    signingCredentials: signinCredentials
+                );
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                return Ok(new { Token = tokenString });
+            }
+            return Unauthorized();
+        }
+    }
+
+}
